2782258e4abc695074c3b6dd61026c755e7cdcf4
Paco Jacquemain <paco.jacquemain@etudiant.univ-reims.fr>
2023-01-03 15:36:26 +0100

    isContenuCorrect : tests ok

--- Model/Cellule.py
+++ Model/Cellule.py
@@ -18,5 +18,18 @@ def type_cellule(cell: dict) -> bool:
     return type(cell) == dict and const.CONTENU in cell and const.VISIBLE in cell \
         and type(cell[const.VISIBLE] == bool) and type(cell[const.CONTENU]) == int \
         and (0 <= cell[const.CONTENU] <= 8 or cell[const.CONTENU] == const.ID_MINE)
-
+def isContenuCorrect(n:int)->bool:
+    contenuCorrect=False
+    valeurPoss = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    u=0
+    if type(n) != int:
+        return False
+    else:
+        while not contenuCorrect and u <9:
+            if valeurPoss[u] == n:
+                contenuCorrect = True
+            u += 1
+        if n == const.ID_MINE:
+            contenuCorrect = True
+        return contenuCorrect
 
